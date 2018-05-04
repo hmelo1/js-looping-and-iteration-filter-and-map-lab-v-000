df17cd2975b8ca29b8da274315bc733faaa8f66c
Eddie Melo <heriberto@melo.nyc>
2018-05-04 01:59:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue){
-
+  return drivers.map(function (driver){
+    if (driver.revenue == revenue){
+      return driver;
+    }
+  })
 }
