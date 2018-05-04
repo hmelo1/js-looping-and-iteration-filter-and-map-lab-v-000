931d10b8585b23adc87878c48af5cc67fab81298
Eddie Melo <heriberto@melo.nyc>
2018-05-04 02:04:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue){
-  return drivers.map(function (driver){
+  var test =  drivers.map(function (driver){
     if (driver.revenue > revenue){
-      console.log(revenue)
+      return driver
     }
   })
+  return test
 }
