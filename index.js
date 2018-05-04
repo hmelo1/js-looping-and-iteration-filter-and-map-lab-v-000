0f786bc963529d427bc80d409d7dfef74582996b
Eddie Melo <heriberto@melo.nyc>
2018-05-04 02:02:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function driversWithRevenueOver(drivers, revenue){
   return drivers.map(function (driver){
     if (driver.revenue > revenue){
-      return driver;
+      console.log(revenue)
     }
   })
 }
